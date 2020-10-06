2b0662d18ae64d19b9053fad821141b64d36347c
Arilson B. Ferreira <arilsonbrj@gmail.com>
2020-10-06 12:07:40 -0300

    Updated Theme

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,6 +1,14 @@
 
-const body = document.body
 
-body.addEventListener("load", () => {
+(function(){
+  console.log("load")
+
+})();
+
+document.addEventListener("DOMContentLoaded", console.log("load"));
+
+const loadEvent = () => {
   console.log("Loaded content.")
-})
+}
+
+window.addEventListener("load", loadEvent)
