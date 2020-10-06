5dfd1e4a317b564a28c28c653d046bd90291ea82
Arilson B. Ferreira <arilsonbrj@gmail.com>
2020-10-06 14:38:41 -0300

    updated

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,8 +1,19 @@
 
 
 (function(){
-  console.log("load")
-
+  const scrollHeader = (e) => {
+    let header = document.querySelector("header")
+    let top = document.documentElement.scrollTop
+    if(top >= window.innerHeight){
+      header.style.height = -100
+      header.style.position = "fixed"
+      header.style.height = 100
+    }else{
+      header.style.position = "relative"
+      header.style.height = 100
+    }
+  }
+  window.addEventListener("scroll", scrollHeader)
 })();
 
 document.addEventListener("DOMContentLoaded", console.log("load"));
